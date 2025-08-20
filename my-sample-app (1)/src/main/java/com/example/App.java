9436d28cbfe2632d423957abe1bc7ae6ccc625b1
numan2310 <pathan23n@gmail.com>
2025-08-20 11:38:54 +0530

    Update App.java

--- my-sample-app (1)/src/main/java/com/example/App.java
+++ my-sample-app (1)/src/main/java/com/example/App.java
@@ -2,7 +2,7 @@ package com.example;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Welcome to My Sample Maven App!");
+        System.out.println("Welcome to maven123");
         Calculator calc = new Calculator();
         int result = calc.add(5, 10);
         System.out.println("5 + 10 = " + result);
